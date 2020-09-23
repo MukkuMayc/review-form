3c61d07c364e6cada20c3c2b9e3e3eb13ad5d2c5
mukkumayc <st063775@student.spbu.ru>
2020-09-23 10:55:30 +0300

    Add error handling, printing message from server

--- handleForm.js
+++ handleForm.js
@@ -4,29 +4,40 @@ console.log(form);
 function handleSubmit(e) {
   e.preventDefault();
   let formData = new FormData(form);
-  fetch('http://localhost:8080/review', {
-    body: JSON.stringify({
-        reviewtext: formData.get('reviewtext')
-    }),
-    headers: {
-        "Content-Type": "application/json",
-    },
-    method: "post"
-  }).then(res => {
-    showMessage(res.ok);
-  })
+  (async () => {
+    var isOk = false;
+    await fetch('http://localhost:8080/review', {
+      body: JSON.stringify({
+          reviewtext: formData.get('reviewtext')
+      }),
+      headers: {
+          "Content-Type": "application/json",
+      },
+      method: "post"
+    })
+    .then(res => {
+      isOk = res.ok;
+      return res.text();
+    })
+    .then(text => {
+      showMessage(isOk, text);
+    })
+    .catch(e => {
+      console.error('Error:', e);
+    });
+  })()
 }
 
 var messageNode;
 
-function showMessage(ok) {
+function showMessage(ok, res) {
   if (messageNode) {
     formWrapper.removeChild(messageNode);
   }
   messageNode = document.createElement("div");
   messageNode.classList.add("alert");
 
-  messageNode.textContent = ok ? "Success!" : "Error!";
+  messageNode.textContent = (ok ? "Success! " : "Error! ") + res;
   messageNode.classList.add(ok ? "alert-success" : "alert-danger");
 
   formWrapper.appendChild(messageNode);
